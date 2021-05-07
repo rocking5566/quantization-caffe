ec1ece4f92606876a64670edb172bcee84de7cf6
rocking <rocking@cvitek.com>
2021-05-07 12:21:57 +0800

    Fix bug of perlayer

--- util/caffe_wrapper.py
+++ util/caffe_wrapper.py
@@ -5,7 +5,7 @@ import numpy as np
 def fp32_int8_init(net, fp32_layers=[], perchanel=True):
     for layer_name in net._layer_names:
         if layer_name not in fp32_layers:
-            net.init_fakequant_int8(layer_name, True)
+            net.init_fakequant_int8(layer_name, perchanel)
 
 def fp32_int8_init_from_file(net, fp32_layers_file, perchanel=True):
     fp32_layers = np.loadtxt(fp32_layers_file, str, delimiter='\t')
