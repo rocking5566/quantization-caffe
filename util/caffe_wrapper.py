6d7724bdeec32b19145d1624099f272d42f23236
rocking <rocking@cvitek.com>
2021-05-07 12:23:26 +0800

    add int8_init() to set all layer to int8

--- util/caffe_wrapper.py
+++ util/caffe_wrapper.py
@@ -2,6 +2,10 @@ from __future__ import division
 import numpy as np
 
 
+def int8_init(net, perchanel=True):
+    for layer_name in net._layer_names:
+        net.init_fakequant_int8(layer_name, perchanel)
+
 def fp32_int8_init(net, fp32_layers=[], perchanel=True):
     for layer_name in net._layer_names:
         if layer_name not in fp32_layers:
