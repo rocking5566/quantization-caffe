97602a42dd39d9eb6b26139671f950603ec48a0f
rocking <rocking@cvitek.com>
2021-05-10 15:55:30 +0800

    Remove redundant empty line

--- util/caffe_wrapper.py
+++ util/caffe_wrapper.py
@@ -19,9 +19,7 @@ def fp32_int8_init(net, quant_info, fp32_layers=[], perchanel=True):
     for layer_name in net._layer_names:
         if layer_name not in fp32_layers:
             net.init_fakequant_int8(layer_name, perchanel)
-
     net.PrintQuantInfo()
-
 
 def fp32_int8_init_from_file(net, quant_info, fp32_layers_file, perchanel=True):
     net.import_activation_range(quant_info)
