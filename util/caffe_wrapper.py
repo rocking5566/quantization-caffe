a03f9272b70273a7bd688b03c3b91486783f4f6b
rocking <rocking@cvitek.com>
2021-05-11 17:16:07 +0800

    Add int4_8, int8, fp32 mix precision api for caffe

--- util/caffe_wrapper.py
+++ util/caffe_wrapper.py
@@ -29,3 +29,20 @@ def fp32_int8_init_from_file(net, quant_info, fp32_layers_file, perchanel=True):
     net.import_activation_range(quant_info)
     fp32_layers = np.loadtxt(fp32_layers_file, str, delimiter='\t')
     fp32_int8_init(net, quant_info, fp32_layers, perchanel)
+
+def quant_init(net, quant_info, int8_layers=[], int4_8_layer=[], perchanel=True):
+    net.import_activation_range(quant_info)
+    for layer_name in net._layer_names:
+        if layer_name in int4_8_layer:
+            net.init_fakequant_int4_8(layer_name, perchanel)
+        elif layer_name in int8_layers:
+            net.init_fakequant_int8(layer_name, perchanel)
+        else:
+            net.set_infer_type_to_native(layer_name)
+    net.PrintQuantInfo()
+
+def quant_init_from_file(net, quant_info, int8_layers_file, int4_8_layer_file, perchanel=True):
+    net.import_activation_range(quant_info)
+    int8_layers = np.loadtxt(int8_layers_file, str, delimiter='\t')
+    int4_8_layer = np.loadtxt(int4_8_layer_file, str, delimiter='\t')
+    quant_init(net, quant_info, int8_layers, int4_8_layer, perchanel)
