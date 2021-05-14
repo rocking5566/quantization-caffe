d4c0e0f12fa94d756abf0d42d2476e7650f81131
rocking <rocking@cvitek.com>
2021-05-14 11:39:01 +0800

    [What] Refactor run_calibration. Rename it as sample

--- calibration_tool/run_calibration_sample.py
+++ calibration_tool/run_calibration_sample.py
@@ -1,19 +1,20 @@
 import os
 import cv2
 from model_path import get_caffe_model_path
-from mobilenet_v1_util import preprocess
+from experiment.imagenet.mobilenet_v1.mobilenet_v1_util import preprocess
 import numpy as np
 from kld_calibrator import KLD_Calibrator
 
 g_image_list = '/workspace/util/dataset/imagenet_imgs.txt'
+model_name = 'mobilenet_v1_0.25'
 
 
 if __name__ == '__main__':
-    proto, weight, _ = get_caffe_model_path('mobilenet_v1_0.25')
+    proto, weight, _ = get_caffe_model_path(model_name)
 
     calibrator = KLD_Calibrator(g_image_list, proto, weight,
         image_count=100, preprocess=preprocess, histogram_bin_num=20480)
 
     threshold_table = calibrator.do_calibration()
     threshold_table = calibrator.threshold_opt(threshold_table)
-    calibrator.dump_threshold_table(threshold_table, 'mobilenet_v1_0.25.threshold_table')
+    calibrator.dump_threshold_table(threshold_table, '{}.threshold_table'.format(model_name))
