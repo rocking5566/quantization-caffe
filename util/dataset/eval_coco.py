e85d73d455211b32311caaae32052edce95446a7
rocking <rocking@cvitek.com>
2021-05-24 13:25:17 +0800

    [What] Add evaluation number [Why] Prepare to use evalation result as loss function of mix precision

--- util/dataset/eval_coco.py
+++ util/dataset/eval_coco.py
@@ -25,7 +25,7 @@ def clip_box(box, image_shape):
     return np.array([bx, by, bw, bh])
 
 
-def coco_evaluation(detect_func):
+def coco_evaluation(detect_func, val_data_count=g_val_data_count):
     coco_ids = [1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 13, 14, 15, 16, 17,
                 18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34, 35, 36,
                 37, 38, 39, 40, 41, 42, 43, 44, 46, 47, 48, 49, 50, 51, 52, 53,
@@ -34,8 +34,8 @@ def coco_evaluation(detect_func):
 
     jdict = []
     img_ids = []
-    pbar = tqdm(coco_generator(generate_count=g_val_data_count),
-                total=g_val_data_count)
+    pbar = tqdm(coco_generator(generate_count=val_data_count),
+                total=val_data_count)
 
     for [img_bgr, image_id] in pbar:
         img_ids.append(image_id)
